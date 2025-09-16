dfdcc5c97683df611c7fd3c6b35a9f854c969b9c
jvn1o <junok0987@naver.com>
2025-09-16 10:28:23 +0900

    몫 구하기

--- Problem_3.java
+++ Problem_3.java
@@ -1,5 +1,5 @@
 public class Problem_3 {
-    public int answer(int num1, int num2) {
+    public int solution(int num1, int num2) {
         int answer = 0;
         answer = division(num1, num2);
         return answer;
