d0dc440e32f65d3b965977706b5c9622be3c64c5
jvn1o <junok0987@naver.com>
2025-09-16 10:20:03 +0900

    두 수의 차 구하기

--- Problem_1.java
+++ Problem_1.java
@@ -1,5 +1,11 @@
 public class Problem_1 {
     public int The_difference_of_two_numbers(int num1, int num2) {
-        return num1- num2;
+        int answer = 0;
+        answer = Subtraction(num1, num2);
+        return answer;
+    }
+
+    public int Subtraction(int num1, int num2) {
+        return num1 - num2;
     }
 }
