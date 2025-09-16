e386a71546d1550da7a25e2f955f2514a05e9d4f
jvn1o <junok0987@naver.com>
2025-09-16 10:22:26 +0900

    몫

--- Problem_3.java
+++ Problem_3.java
@@ -1,5 +1,5 @@
 public class Problem_3 {
-    public int Calculate_the_quotient_of_two_numbers(int num1, int num2) {
+    public int answer(int num1, int num2) {
         int answer = 0;
         answer = division(num1, num2);
         return answer;
