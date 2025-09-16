07e3536df31175013aba64eddc133fb0ec17ce4e
jvn1o <junok0987@naver.com>
2025-09-16 10:18:22 +0900

    몫 구하기

---
+++ Problem_3.java
@@ -0,0 +1,11 @@
+public class Problem_3 {
+    public int Calculate_the_quotient_of_two_numbers(int num1, int num2) {
+        int answer = 0;
+        answer = division(num1, num2);
+        return answer;
+    }
+
+    public int division(int num1, int num2) {
+        return num1 / num2;
+    }
+}
