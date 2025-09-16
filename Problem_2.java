b906481d1c56f547af34dcf94f7da399d2b2e4fc
jvn1o <junok0987@naver.com>
2025-09-16 10:19:08 +0900

    두 수의 곱 구하기

--- Problem_2.java
+++ Problem_2.java
@@ -1,5 +1,11 @@
 public class Problem_2 {
     public int The_product_of_two_numbers(int num1, int num2) {
+        int answer = 0;
+        answer = Multiplication(num1, num2);
+        return answer;
+    }
+
+    public int Multiplication(int num1, int num2) {
         return num1 * num2;
     }
 }
