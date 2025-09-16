68d84113b7dcdd5b69a4679446edbcbd73d3db9a
jvn1o <junok0987@naver.com>
2025-09-16 10:22:16 +0900

    두 수의 곱

--- Problem_2.java
+++ Problem_2.java
@@ -1,5 +1,5 @@
 public class Problem_2 {
-    public int The_product_of_two_numbers(int num1, int num2) {
+    public int answer(int num1, int num2) {
         int answer = 0;
         answer = Multiplication(num1, num2);
         return answer;
