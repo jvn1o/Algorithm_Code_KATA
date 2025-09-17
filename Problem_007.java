7e6d655d5a2ba61d8f403e3eb155707c8bb32269
jvn1o <junok0987@naver.com>
2025-09-17 10:31:56 +0900

    두 수의 나눗셈

---
+++ Problem_007.java
@@ -0,0 +1,12 @@
+public class Problem_007 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = TheQuotientMultipliedBy1000(num1, num2);
+        return answer;
+    }
+
+    public int TheQuotientMultipliedBy1000(int num1, int num2) {
+        double answer = (double) num1 / (double) num2;
+        return (int) (answer * 1000);
+    }
+}
