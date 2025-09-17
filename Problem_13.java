be99889d0c41c355655f83a13dfcd45789d86cc3
jvn1o <junok0987@naver.com>
2025-09-17 10:18:43 +0900

    자릿수 더하기

---
+++ Problem_13.java
@@ -0,0 +1,14 @@
+public class Problem_13 {
+    public int solution(int n) {
+        int answer = 0;
+        answer = digitSum(n);
+        return answer;
+    }
+
+    public int digitSum(int n) {
+        return String.valueOf(n)
+                .chars()
+                .map(c -> c - '0')
+                .sum();
+    }
+}
