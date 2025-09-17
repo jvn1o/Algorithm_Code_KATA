205781e3ae995775d7466e2613c0761250cb8973
jvn1o <junok0987@naver.com>
2025-09-17 10:31:35 +0900

    몫 구하기

---
+++ Problem_003.java
@@ -0,0 +1,11 @@
+public class Problem_003 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = division(num1, num2);
+        return answer;
+    }
+
+    public int division(int num1, int num2) {
+        return num1 / num2;
+    }
+}
