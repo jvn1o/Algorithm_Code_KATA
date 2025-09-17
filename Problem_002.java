b8989e5c00333eda0572454727426b700a3ff68c
jvn1o <junok0987@naver.com>
2025-09-17 10:31:29 +0900

    두 수의 곱

---
+++ Problem_002.java
@@ -0,0 +1,11 @@
+public class Problem_002 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = Multiplication(num1, num2);
+        return answer;
+    }
+
+    public int Multiplication(int num1, int num2) {
+        return num1 * num2;
+    }
+}
