bd1c91eaaab3e75bddbcca6fe7396244ec9576de
jvn1o <junok0987@naver.com>
2025-09-17 10:31:51 +0900

    두 수의 합

---
+++ Problem_006.java
@@ -0,0 +1,11 @@
+public class Problem_006 {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 + num2;
+        return answer;
+    }
+
+    public int Sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
